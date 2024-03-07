6acbad9d0a44acd6426f6dce729a17e3c56073e4
Ivana <ivana.a.maldonado@gmail.com>
2024-03-07 02:23:19 -0500

    rename variables folder_path and clean_folder for clarity

--- desktop-organizer.py
+++ desktop-organizer.py
@@ -1,14 +1,15 @@
 import os #library to interact with the operating system
 import shutil  #shutil library to move files
 
+
 if __name__ == "__main__":
     print("Desktop Organizer Script")
-    # path to downloads folder
-    folder_path = '/PATH/TO/FOLDER'
+    # path to the folder to be cleaned
+    target_folder_path = 'PATH_TO_FOLDER_TO_BE_CLEANED'
     # validate whether folder path is a real path, if so run the clean funtion, esle return error message. 
     if os.path.isdir(folder_path):
-        clean_folder(folder_path)
-        # Need to write function to clean the folder
+        clean_folder(folder_path) 
+        # NEED TO CREATE CLEAN FOLDER FUNCTION
     else:
         print("Folder path is not valid. Please enter a valid folder path.")
         
